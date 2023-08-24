4f54682ceeed6b73534753f9d8e6822612135185
Henree001 <okaforhenry779@gmail.com>
2023-08-24 14:19:27 +0100

    fixed issue of some test cases failing

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -8,16 +8,17 @@ def canUnlockAll(boxes):
     (indices) to other boxes can be unlocked given that the first
     box is unlocked.
     '''
-    box_length = set(range(1, len(boxes)))
-    # print(len(box_length))
+    box_keys = set(range(1, len(boxes)))
 
     for index, box in enumerate(boxes):
-        if index == len(boxes) - 2 and len(box_length) != 0:
-            if not box_length.issubset(box):
-                break
+        #     if index == len(boxes) - 2 and len(box_keys) != 0:
+        #         if not box_keys.issubset(box):
+        #             break
 
-        for index, key in enumerate(box):
-            if key in box_length:
-                box_length.remove(key)
+        for key in box:
+            if key == index:
+                continue
+            if key in box_keys:
+                box_keys.remove(key)
 
-    return len(box_length) == 0
+    return len(box_keys) == 0
