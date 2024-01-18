c5e4566010be276d0fe3aadbe2ae1a4472dc2f0c
Henree001 <okaforhenry779@gmail.com>
2024-01-18 16:44:09 +0100

    redid the task

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,32 +1,36 @@
 #!/usr/bin/python3
-"""
-This module defines the function validUTF8.
-"""
+"""This module defines the function validUTF8."""
+
+
 def validUTF8(data):
     """Determines if a given data set
     represents a valid utf-8 encoding
     """
+    number_bytes = 0
     
-    # Count of remaining continuation bytes
-    remaining_bytes = 0
-    
-    for byte in data:
-        # Check if the current byte is a continuation byte
-        if remaining_bytes > 0:
-            # Check if the two most significant bits are '10'
-            if (byte & 0b11000000) != 0b10000000:
+    for i in data:
+
+        mask_byte = 0b10000000
+
+        if number_bytes == 0:
+
+            while i & mask_byte:
+                number_bytes += 1
+                mask_byte >>= 1
+
+            if number_bytes == 0:
+                continue
+
+            if number_bytes == 1 or number_bytes > 4:
                 return False
-            remaining_bytes -= 1
+
         else:
-            # Count the number of leading 1s to determine the character length
-            mask = 0b10000000
-            while (byte & mask) and mask > 0b00100000:
-                remaining_bytes += 1
-                mask >>= 1
-
-            # Handle the case where the character length is 1
-            if remaining_bytes == 1 or remaining_bytes > 4:
+            if (i & 0b11000000) != 0b10000000:
                 return False
 
-    # Check if there are any remaining continuation bytes
-    return remaining_bytes == 0
+        number_bytes -= 1
+
+
+    return True
+
+    
