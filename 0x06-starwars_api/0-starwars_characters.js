aa3c0ff55604782807c83f3cc83e291bd9cb4130
Henree001 <okaforhenry779@gmail.com>
2024-02-14 11:08:33 +0100

    fixed indent error from semi- standard

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -9,26 +9,25 @@
 const request = require('request');
 
 const makeRequest = (url) => {
-    return new Promise((resolve, reject) => {
-        request.get(url, (err, response, body) => {
-            if (err) {
-                reject(err);
-            } else {
-                resolve(body);
-            }
-        });
+  return new Promise((resolve, reject) => {
+    request.get(url, (err, response, body) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(body);
+      }
     });
+  });
 };
 
 const getCharacters = async () => {
+  const films = await makeRequest(`https://swapi-api.alx-tools.com/api/films/${process.argv[2]}/`);
+  const characters = JSON.parse(films).characters;
 
-    const films = await makeRequest(`https://swapi-api.alx-tools.com/api/films/${process.argv[2]}/`);
-    const characters = JSON.parse(films).characters;
-
-    for (let i = 0; i < characters.length; i++) {
-        const character = await makeRequest(characters[i]);
-        console.log(JSON.parse(character).name);
-    }
-}
+  for (let i = 0; i < characters.length; i++) {
+    const character = await makeRequest(characters[i]);
+    console.log(JSON.parse(character).name);
+  }
+};
 
 getCharacters();
